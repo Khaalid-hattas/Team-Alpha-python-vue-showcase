5dd6f3c3533d7042e96becda8619b8ad7d762831
Khaalid-hattas <khaalidhattas123@gmail.com>
2026-08-07 08:13:05 +0200

    added route/api route as a fix.3

--- mypart/src/services/api.js
+++ mypart/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://team-alpha-python-vue-showcase-back.onrender.com/',
   timeout: 8000,
 })
 
